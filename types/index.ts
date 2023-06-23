44d81007240cf6e386e8230053ac36eedaf6c484
Muhammad  Muqorrobin <muhammad.muqorrobin@mitracomm.com>
2023-06-23 16:54:08 +0700

    CarHubJSM-start

---
+++ types/index.ts
@@ -0,0 +1,17 @@
+
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+    isDisabled?: boolean;
+    btnType?: "button" | "submit";
+    containerStyles?: string;
+    textStyles?: string;
+    title: string;
+    rightIcon?: string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface SearchManuFacturerProps {
+    manufacturer: string;
+    setManuFacturer: (manufacturer: string) => void;
+}
