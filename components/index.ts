fc6c3a2aa0f3c3b68c0ec193dca2cad39bca1bb8
urobin84 <urobin84@gmail.com>
2023-06-25 19:22:20 +0700

    final carhub jsm

--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,9 @@ import Footer from "./Footer"
 import CustomFilter from "./CustomFilter"
 import SearchBar from "./SearchBar"
 import SearchManufacturer from "./SearchManufacturer"
+import CarCard from "./CarCard"
+import CarDetails from "./CarDetails"
+import ShowMore from "./ShowMore"
 
 export {
     Hero,
@@ -13,5 +16,8 @@ export {
     Footer,
     CustomFilter,
     SearchBar,
-    SearchManufacturer
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
+    ShowMore
 }
--- types/index.ts
+++ types/index.ts
@@ -15,3 +15,45 @@ export interface SearchManuFacturerProps {
     manufacturer: string;
     setManuFacturer: (manufacturer: string) => void;
 }
+
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+}
+
+export interface FilterProps {
+    manufacturer?: string;
+    year?: number;
+    model?: string;
+    limit?: number;
+    fuel?: string;
+}
+
+export interface HomeProps {
+    searchParams: FilterProps;
+}
+
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+
+export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+}
+
+export interface ShowMoreProps {
+    pageNumber: number;
+    isNext: boolean;
+}
